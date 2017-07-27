2714803db568085db7e5f9a854c3ffd191240f0d
Xander <xander.madsen@gmail.com>
2017-07-27 13:35:34 -0400

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,9 +1,7 @@
 def fibonacci(n):
     """Calculate the nth Fibonacci number"""
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+    if n == 0 or n == 1:
+        return n
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
